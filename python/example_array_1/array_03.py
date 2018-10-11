1ac7acae543afe2d35f830dacf69a47bec38318e
Bruno Reboucas <kalle.knochen@gmail.com>
2018-10-11 19:36:08 +0200

    More array examples

---
+++ python/example_array_1/array_03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+print("array_03")
+
+###
+### Write a Python program to reverse the order of the items in the array
+###
+
+arr_1 = [
+    'a',
+    'b',
+    'c',
+    'd'
+]
+
+print("arr_1: %s" % arr_1 )
+arr_1.reverse()
+print("arr_1: %s" % arr_1 )
